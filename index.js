add1bf54f5dad75d9982f5535c7ec8e5ef77ac3e
amitpaultl <amitpaultl@gmail.com>
2022-11-08 20:57:38 +0600

    service post add

---
+++ index.js
@@ -0,0 +1,85 @@
+const express = require('express');
+const cors = require('cors');
+const { MongoClient, ServerApiVersion } = require('mongodb');
+
+require('dotenv').config();
+const app = express();
+const port = process.env.PROT || 5000
+
+// middleware
+app.use(cors());
+app.use(express.json());
+
+
+
+// password: psychologist
+// user: rEusKmzaFYW7toMV
+
+
+
+
+const uri = `mongodb+srv://${process.env.USER}:${process.env.PASSWORD}@cluster0.acij04d.mongodb.net/?retryWrites=true&w=majority`;
+const client = new MongoClient(uri, { useNewUrlParser: true, useUnifiedTopology: true, serverApi: ServerApiVersion.v1 });
+
+const dbConnent = async () => {
+    try {
+        
+        await client.connect();
+
+    } catch (error) {
+        console.log(error);
+    }
+}
+dbConnent();
+
+// database name
+const service = client.db('psychologist').collection('service');
+
+// service add server
+app.post("/service", async (req, res) => {
+    try {
+        const result = await service.insertOne(req.body);
+        if (result.insertedId) {
+            res.send({
+              success: true,
+              message: `Successfully created the ${req.body.title}`,
+            });
+        }else {
+            res.send({
+              success: false,
+              error: "Couldn't create the product",
+            });
+        }
+    } catch (error) {
+        res.send({
+          success: false,
+          error: error.message,
+        });
+    }
+})
+
+
+
+
+
+
+
+
+
+// running server
+app.get('/',(req,res)=>{
+    res.send('Psychologist server is running')
+})
+
+
+// log listen
+app.listen(port, () => {
+    client.connect(err => {
+        if (err) {
+            console.log(err);
+        }
+        console.log('Mongodb connect');
+       
+    });
+    console.log('volunteer server is running ' + port);
+})
